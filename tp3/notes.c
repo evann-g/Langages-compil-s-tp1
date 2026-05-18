6897dcd9b311269a25f182fd491da8d81f785733
evann-g <evann.guilhem@ynov.com>
2026-05-18 12:01:25 +0200

    tp3

--- tp3/notes.c
+++ tp3/notes.c
@@ -5,7 +5,17 @@ int main(void) {
     scanf("%d", &note);
     if (note < 0 || note > 20) {
         printf("Note invalide\n");
-    } else if (note > 10) {
+    } else if (note >= 16) {
+    printf("Tres bien\n");
+    } else if (note >= 12) {
+    printf("Bien\n");
+    } else if (note >= 10) {
+    printf("Passable\n");
+    } else {
+    printf("Insuffisant\n");
+    }
+    
+    if (note > 10) {
         printf("Admis\n");
     } else {
         printf("Non admis\n");
